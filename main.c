917e31ffa3cd81b708745b1593e08233073dac72
ilyan <ilyanbendib@gmail.com>
2023-12-07 18:49:24 +0100

    le perso bougee

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ilyanbendib <ilyanbendib@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 17:48:40 by ilbendib          #+#    #+#             */
-/*   Updated: 2023/12/07 17:38:25 by ilyanbendib      ###   ########.fr       */
+/*   Updated: 2023/12/07 18:48:58 by ilyanbendib      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,99 +14,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// typedef struct	s_data {
-// 	void	*img;
-// 	char	*addr;
-// 	int		bits_per_pixel;
-// 	int		line_length;
-// 	int		endian;
-// }				t_data;
-
-// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-// {
-// 	char	*dst;
-
-// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-// 	*(unsigned int*)dst = color;
-// }
-
-// typedef struct	s_vars {
-// 	void	*mlx;
-// 	void	*win;
-// }				t_vars;
-
-// int	close(t_vars *vars)
-// {
-// 	mlx_destroy_window(vars->mlx, vars->win);
-// 	return (0);
-// }
-
-// int	main(void)
-// {
-// 	t_vars	vars;
-// 	void	*mlx;
-// 	void	*mlx_win;
-// 	t_data	img;
-
-// 	mlx = mlx_init();
-// 	mlx_win = mlx_new_window(mlx, 1920, 1080, "ilyan game");
-// 	img.img = mlx_new_image(mlx, 1920, 1080);
-// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-// 								&img.endian);
-// 	my_mlx_pixel_put(&img, 5, 5, 0x000000FF);
-
-// 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-// 	mlx_loop(mlx);
-// 	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
-// 	mlx_loop(vars.mlx);
-// }
+typedef		struct s_game
+{
+	void *mlx;
+    void *win;
+	void	*ground_img;
+	void	*character_img;
+	void	*Wall_img;
+	void	*Collectibles_img;
+	void	*exit_img;
+	int		character_x;
+	int		character_y;
+}	t_game;
 
 #define KEY_PRESS 2
 #define KEY_RELEASE 3
 #define KEY_PRESS_MASK (1L<<0)
 #define ESCAPE_KEY 53
+#define TITLE_SIZE 48
 
-int handle_key_press(int keycode)
+void draw_character(t_game *game)
 {
+    mlx_put_image_to_window(game->mlx, game->win, game->character_img, game->character_x, game->character_y);
+}
+
+int handle_key_press(int keycode, void *param)
+{
+	t_game *game = (t_game *)param;
+	
     if (keycode == ESCAPE_KEY)
     {
         printf("Programme terminé.\n");
         exit(0);
     }
-    else
+    else if (keycode == 0)
     {
-        printf("Key pressed: %d\n", keycode);
+		game->character_x -= TITLE_SIZE;
+        draw_character(game);
+    }
+	else if (keycode == 13)
+    {
+		game->character_y -= TITLE_SIZE;
+        draw_character(game);
+    }
+	else if (keycode == 2)
+    {
+		game->character_x += TITLE_SIZE;
+    	draw_character(game);
+    }
+	else if (keycode == 1)
+    {
+		game->character_y += TITLE_SIZE;
+        draw_character(game);
     }
     return 0;
 }
 
 int main(void)
 {
-    void *mlx;
-    void *win;
-	void	*ground_img = NULL;
-	void	*character_img = NULL;
-	void	*Wall_img = NULL;
-	void	*Collectibles_img = NULL;
-	void	*exit_img = NULL;
-	int	size = 48;
+	t_game game;
+	int img_width, img_height;
+	
+    game.mlx = mlx_init();
+    game.win = mlx_new_window(game.mlx, 800, 600, "Example");
+    mlx_hook(game.win, KEY_PRESS, KEY_PRESS_MASK, handle_key_press, NULL);
+	
+	game.ground_img = mlx_xpm_file_to_image(game.mlx, "img/ground.xpm", &img_width, &img_height);
+	game.character_img = mlx_xpm_file_to_image(game.mlx, "img/character.xpm", &img_width, &img_height);
+	game.Wall_img = mlx_xpm_file_to_image(game.mlx, "img/Wall.xpm", &img_width, &img_height);
+	game.Collectibles_img = mlx_xpm_file_to_image(game.mlx, "img/Colectibles.xpm", &img_width, &img_height);
+	game.exit_img = mlx_xpm_file_to_image(game.mlx, "img/exit.xpm", &img_width, &img_height);
+	
+	// mlx_put_image_to_window(game.mlx, game.win, game.character_img, 250, 250);
+	mlx_put_image_to_window(game.mlx, game.win, game.Collectibles_img, 150, 150);
+	mlx_put_image_to_window(game.mlx, game.win, game.exit_img, 500, 500);
+	mlx_put_image_to_window(game.mlx, game.win, game.Wall_img, 350, 350);
+	mlx_put_image_to_window(game.mlx, game.win, game.ground_img, 0, 0);
+    // mlx_loop(game.mlx);
 
-    mlx = mlx_init();
-    win = mlx_new_window(mlx, 800, 600, "Example");
-    mlx_hook(win, KEY_PRESS, KEY_PRESS_MASK, handle_key_press, NULL);
+	game.character_x = 250;
+	game.character_y = 250;
 	
-	ground_img = mlx_xpm_file_to_image(mlx, "img/ground.xpm", &size, &size);
-	character_img = mlx_xpm_file_to_image(mlx, "img/character.xpm", &size, &size);
-	Wall_img = mlx_xpm_file_to_image(mlx, "img/Wall.xpm", &size, &size);
-	Collectibles_img = mlx_xpm_file_to_image(mlx, "img/Colectibles.xpm", &size, &size);
-	exit_img = mlx_xpm_file_to_image(mlx, "img/exit.xpm", &size, &size);
-	
-	mlx_put_image_to_window(mlx, win, character_img, 250, 250);
-	mlx_put_image_to_window(mlx, win, Collectibles_img, 150, 150);
-	mlx_put_image_to_window(mlx, win, exit_img, 500, 500);
-	mlx_put_image_to_window(mlx, win, Wall_img, 350, 350);
-	mlx_put_image_to_window(mlx, win, ground_img, 0, 0);
-    mlx_loop(mlx);
+	draw_character(&game);
+	mlx_hook(game.win, 2, 1L << 0, handle_key_press, &game);
+	mlx_loop(game.mlx);
 	
     return 0;
 }
