f1f5629cc5977fb4772e8624aa84163aa344ceea
ilyan <ilyanbendib@gmail.com>
2023-12-15 16:23:56 +0100

    commit

--- game/ft_parsing_map.c
+++ game/ft_parsing_map.c
@@ -6,7 +6,7 @@
 /*   By: ilbendib <ilbendib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 15:28:56 by ilbendib          #+#    #+#             */
-/*   Updated: 2023/12/14 15:38:56 by ilbendib         ###   ########.fr       */
+/*   Updated: 2023/12/15 16:21:30 by ilbendib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@ void size_map(t_game *game)
 	int x;
 	int y;
 
-	y = -1;
-	while(game->map[++y])
+	y = 0;
+	while(game->map[y])
 	{
-		x = -1;
-		while(game->map[y][++x])
+		x = 0;
+		while(game->map[y][x])
 		{
-			game->size_x = x;
+			x++;
 		}
-		game->size_y = y;
+		y++;
 	}
+	game->size_x = x;
+	game->size_y = y;
 }
 
 void ft_parsing_map(t_game *game)
@@ -48,14 +50,20 @@ void ft_parsing_map(t_game *game)
 	fd = open("./map/map2.ber", O_RDONLY);
 	tmp = get_next_line(fd);
 	game->map = malloc(sizeof(char*) * (game->map_height + 1));
-
 	int i = 0;
+	
 	while (tmp && i < game->map_height)
 	{
 		game->map[i] = tmp;
-		tmp = get_next_line(fd);
+		tmp = get_next_line(fd);	
 		i++;
 	}
 	game->map[i] = NULL;
+	
+	ft_printf("------------------------ICI--------------------\n");
+	for (int y = 0; y < i; y++ )
+	{
+		ft_printf("%s", game->map[y]);
+	}
 	close(fd);
 }
