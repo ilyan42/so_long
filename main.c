5c1bf8c83d94c93b99818a272ab560cd62a1a7ca
ilyan <ilyanbendib@gmail.com>
2023-12-20 19:43:38 +0100

    norm

--- main.c
+++ main.c
@@ -6,35 +6,33 @@
 /*   By: ilbendib <ilbendib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 17:48:40 by ilbendib          #+#    #+#             */
-/*   Updated: 2023/12/20 18:21:25 by ilbendib         ###   ########.fr       */
+/*   Updated: 2023/12/20 19:43:05 by ilbendib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-	t_game game;
-	
+	t_game	game;
+
 	ft_parsing_map(&game);
 	size_map(&game);
 	ft_game_init(&game);
 	nb_colect_in_map(&game);
 	ft_print_map(&game);
 	ft_init_player(&game);
-
 	ft_pos_monster_blue(&game);
 	ft_pos_monster_green(&game);
 	ft_pos_monster_red(&game);
-
 	ft_draw_pacman_down(&game);
 	ft_draw_monster_blue(&game);
 	ft_draw_monster_green(&game);
 	ft_draw_monster_red(&game);
-
 	mlx_loop_hook(game.mlx, ft_update, (void *)&game);
-	mlx_hook(game.win, KEY_PRESS, KEY_PRESS_MASK, ft_handle_key_press, (void *)&game);
+	mlx_hook(game.win, KEY_PRESS, KEY_PRESS_MASK, ft_handle_key_press,
+		(void *)&game);
 	mlx_loop(game.mlx);
-	return 0;
+	return (0);
 }
--- so_long.h
+++ so_long.h
@@ -6,30 +6,29 @@
 /*   By: ilbendib <ilbendib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 11:49:36 by ilbendib          #+#    #+#             */
-/*   Updated: 2023/12/20 19:20:39 by ilbendib         ###   ########.fr       */
+/*   Updated: 2023/12/20 19:38:10 by ilbendib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "minilibx-linux/mlx.h"
-#include <fcntl.h>
-#include "LIBFT/libft.h"
+# include "LIBFT/libft.h"
+# include "minilibx-linux/mlx.h"
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-
-typedef		struct s_pos
+typedef struct s_pos
 {
-	int x;
-	int y;
-}		t_pos;
+	int		x;
+	int		y;
+}			t_pos;
 
-typedef		struct s_game
+typedef struct s_game
 {
-	void *mlx;
-	void *win;
+	void	*mlx;
+	void	*win;
 	void	*Pacman_gnd;
 	t_pos	*Pacman;
 	void	*Pacman_wall;
@@ -53,97 +52,90 @@ typedef		struct s_game
 	int		monster_red_direction;
 	t_pos	*Monster_green;
 	t_pos	*Monster_red;
-}	t_game;
+}			t_game;
 
+# define KEY_PRESS 2
+# define KEY_RELEASE 3
+# define KEY_PRESS_MASK (1L << 0)
+# define ESCAPE_KEY 65307
+# define SIZE 50
 
-#define KEY_PRESS 2
-#define KEY_RELEASE 3
-#define KEY_PRESS_MASK (1L<<0)
-#define ESCAPE_KEY 65307
-#define SIZE 50
+# define W 119
+# define S_QW 115
+# define A 97
+# define D_QW 100
 
-#define W 119
-#define S_QW 115
-#define A 97
-#define D_QW 100
+# define Z 13
+# define Q 1
+# define S 0
+# define D 2
 
-#define Z 13
-#define Q 1
-#define S 0
-#define D 2
-
-void	ft_draw_pacman_down(t_game *game);
-void	ft_draw_pacman_up(t_game *game);
-void	ft_draw_pacman_left(t_game *game);
-int		ft_handle_key_press(int keycode, void *param);
-void	ft_draw_pacman_right(t_game *game);
-void	ft_print_map(t_game *game);
-void	ft_print_map(t_game *game);
-void	ft_new_position(int x, int y, t_game *game);
-void	ft_init_player(t_game *game);
-void	ft_game_init(t_game *game);
-void	ft_parsing_map(t_game *game);
-int		ft_update(t_game *game);
-void	size_map(t_game *game);
-int		move_is_good(t_game *game, int x, int y);
-void	ft_move_left(t_game *game);
-void	ft_move_right(t_game *game);
-void	ft_move_down(t_game *game);
-void	ft_move_up(t_game *game);
-void	ft_print_map(t_game *game);
-int		nb_colec_is_good(t_game *game);
-void	nb_colect_in_map(t_game *game);
-void	ft_print_movements(t_game *game);
-void	print_element(t_game *game, int x, int y, char element);
-void print_blue_monster(t_game *game, int x, int y);
-void print_green_monster(t_game *game, int x, int y);
-void print_red_monster(t_game *game, int x, int y);
-void print_exit(t_game *game, int x, int y);
-void print_collectible(t_game *game, int x, int y);
-void print_ground(t_game *game, int x, int y);
-void print_wall(t_game *game, int x, int y);
-void initialize_game(t_game *game);
-void load_images(t_game *game, int img_size);
-void initialize_window(t_game *game, int img_size);
-void allocate_and_fill_map(t_game *game, int fd);
-void count_map_height(t_game *game, int fd);
-
-
+void		ft_draw_pacman_down(t_game *game);
+void		ft_draw_pacman_up(t_game *game);
+void		ft_draw_pacman_left(t_game *game);
+int			ft_handle_key_press(int keycode, void *param);
+void		ft_draw_pacman_right(t_game *game);
+void		ft_print_map(t_game *game);
+void		ft_print_map(t_game *game);
+void		ft_new_position(int x, int y, t_game *game);
+void		ft_init_player(t_game *game);
+void		ft_game_init(t_game *game);
+void		ft_parsing_map(t_game *game);
+int			ft_update(t_game *game);
+void		size_map(t_game *game);
+int			move_is_good(t_game *game, int x, int y);
+void		ft_move_left(t_game *game);
+void		ft_move_right(t_game *game);
+void		ft_move_down(t_game *game);
+void		ft_move_up(t_game *game);
+void		ft_print_map(t_game *game);
+int			nb_colec_is_good(t_game *game);
+void		nb_colect_in_map(t_game *game);
+void		ft_print_movements(t_game *game);
+void		print_element(t_game *game, int x, int y, char element);
+void		print_blue_monster(t_game *game, int x, int y);
+void		print_green_monster(t_game *game, int x, int y);
+void		print_red_monster(t_game *game, int x, int y);
+void		print_exit(t_game *game, int x, int y);
+void		print_collectible(t_game *game, int x, int y);
+void		print_ground(t_game *game, int x, int y);
+void		print_wall(t_game *game, int x, int y);
+void		initialize_game(t_game *game);
+void		load_images(t_game *game, int img_size);
+void		initialize_window(t_game *game, int img_size);
+void		allocate_and_fill_map(t_game *game, int fd);
+void		count_map_height(t_game *game, int fd);
 
 /*******************Monster_Blue*******************/
 
-void	ft_move_monster_left_blue(t_game *game);
-void	ft_new_position_monster_blue(int x, int y, t_game *game);
-int		move_monster_is_good(t_game *game, int x, int y);
-void	ft_pos_monster_blue(t_game *game);
-void	ft_draw_monster_blue(t_game *game);
-void	ft_move_monster_right_blue(t_game *game);
-void	ft_move_monster_blue(t_game *game);
-void	ft_move_monster_up_blue(t_game *game);
-void	ft_move_monster_down_blue(t_game *game);
-void	ft_move_monster_blue_utils(t_game *game);
-void ft_move_monster_vertical_blue(t_game *game);
-void ft_move_monster_horizontal_blue(t_game *game);
+void		ft_move_monster_left_blue(t_game *game);
+void		ft_new_position_monster_blue(int x, int y, t_game *game);
+int			move_monster_is_good(t_game *game, int x, int y);
+void		ft_pos_monster_blue(t_game *game);
+void		ft_draw_monster_blue(t_game *game);
+void		ft_move_monster_right_blue(t_game *game);
+void		ft_move_monster_blue(t_game *game);
+void		ft_move_monster_up_blue(t_game *game);
+void		ft_move_monster_down_blue(t_game *game);
+void		ft_move_monster_vertical_blue(t_game *game);
+void		ft_move_monster_horizontal_blue(t_game *game);
 
 /*******************Monster_Green*******************/
 
-void ft_draw_monster_green(t_game *game);
-void ft_pos_monster_green(t_game *game);
-void ft_new_position_monster_green(int x, int y, t_game *game);
-void ft_move_monster_down_green(t_game *game);
-void ft_move_monster_up_green(t_game *game);
-void ft_move_monster_green(t_game *game);
+void		ft_draw_monster_green(t_game *game);
+void		ft_pos_monster_green(t_game *game);
+void		ft_new_position_monster_green(int x, int y, t_game *game);
+void		ft_move_monster_down_green(t_game *game);
+void		ft_move_monster_up_green(t_game *game);
+void		ft_move_monster_green(t_game *game);
 
 /*******************Monster_red*******************/
 
-void ft_move_monster_left_red(t_game *game);
-void ft_move_monster_right_red(t_game *game);
-void	ft_move_monster_red(t_game *game);
-void ft_draw_monster_red(t_game *game);
-void ft_pos_monster_red(t_game *game);
-void ft_new_position_monster_red(int x, int y, t_game *game);
-void	ft_move_monster_down_red(t_game *game);
-void	ft_move_monster_up_red(t_game *game);
-
+void		ft_move_monster_left_red(t_game *game);
+void		ft_move_monster_right_red(t_game *game);
+void		ft_move_monster_red(t_game *game);
+void		ft_draw_monster_red(t_game *game);
+void		ft_pos_monster_red(t_game *game);
+void		ft_new_position_monster_red(int x, int y, t_game *game);
 
 #endif
